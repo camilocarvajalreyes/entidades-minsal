3df9f87f04febd08f2c71cdf400203eb41c241a6
camilocarvajalreyes <camilo.carvajal3009@gmail.com>
2022-12-18 23:19:57 -0300

    updte demo version mejorada

--- demo_minsal/predicciones.py
+++ demo_minsal/predicciones.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append('../modelos')
 from transformers import AutoModelForTokenClassification, AutoTokenizer
 from auxfunctions import eval_text, map_entities
+import pandas as pd
 
 ner_dict = {'O': 0,
             'B-ACTIVE_PRINCIPLE': 1,
@@ -70,13 +71,43 @@ def get_admin(entidades:list,texto:str) -> str:
     return ' '.join([token for i, token in enumerate(texto.split(' ')) if i in indices]), indices
 
 
-def etiquetar(texto):
+def render_pandas(ents,text_list):
+    data = {'ACTIVE_PRINCIPLE':'','FORMA_FARMA':'','CANT-ADMIN':'','UND-ADMIN':'','VIA-ADMIN':'','PERIODICITY':'','DURATION':''}
+
+    for i, ent in enumerate(ents):
+        if '-ACTIVE_PRINCIPLE' in ent:
+            data['ACTIVE_PRINCIPLE'] += ' ' + text_list[i]
+        elif '-FORMA_FARMA' in ent:
+            data['FORMA_FARMA'] += ' ' + text_list[i]
+        elif '-CANT' in ent:
+            data['CANT-ADMIN'] += ' ' + text_list[i]
+        elif '-UND' in ent:
+            data['UND-ADMIN'] += ' ' + text_list[i]
+        elif '-VIA_ADMIN' in ent:
+            data['VIA-ADMIN'] += ' ' + text_list[i]
+        elif '-PERIODICITY' in ent:
+            data['PERIODICITY'] += ' ' + text_list[i]
+        elif '-DURATION' in ent:
+            data['DURATION'] += ' ' + text_list[i]
+
+    # df = pd.DataFrame([dict(zip(ents,text_list))])
+    df = pd.DataFrame([data])
+    # df = df.style.set_table_styles([dict(selector='th', props=[('text-align', 'center')])])
+    # df = df.set_properties(**{'text-align': 'center'}).hide_index()
+    # df = df.style.hide(axis='index')
+    return df.style.set_table_styles([dict(selector='th', props=[('text-align', 'center')])]).hide(axis='index')
+
+
+def etiquetar(texto,pandas=True):
     entidades = map_entities(eval_text(texto,tokenizer,model),ner_dict)
     admin_text, indices = get_admin(entidades,texto)
     entidades_admin = map_entities(eval_text(admin_text,tokenizer_admin,model_admin),admin_ner_dict)
     for i, ent_admin in enumerate(entidades_admin):
         entidades[indices[i]] = ent_admin
-    string_ent = '\t'.join(entidades)
-    string_pre = '\t'.join(texto.split())
-    print(string_ent)
-    print(string_pre)
+    if pandas:
+        return render_pandas(entidades,texto.split())
+    else:
+        string_ent = '\t'.join(entidades)
+        string_pre = '\t'.join(texto.split())
+        print(string_ent)
+        print(string_pre)
